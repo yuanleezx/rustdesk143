5d3e93075ac3e473470066adf5b0cfeb0aca4e42
yuanleezx <113503387+yuanleezx@users.noreply.github.com>
2025-11-23 20:10:50 +0800

    cn.rs

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -7,7 +7,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Password", "密码"),
         ("Ready", "就绪"),
         ("Established", "已建立"),
-        ("connecting_status", "正在接入 RustDesk 网络..."),
+        ("connecting_status", "正在接入宝鸡东站专用服务器..."),
         ("Enable service", "允许服务"),
         //修复隐藏托盘图标功能：
         ("Hide Tray", "隐藏托盘图标"),
@@ -46,7 +46,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("id_change_tip", "只可以使用字母 a-z, A-Z, 0-9, - (dash), _ (下划线)。首字母必须是 a-z, A-Z。长度在 6 与 16 之间。"),
         ("Website", "网站"),
         ("About", "关于"),
-        ("Slogan_tip", "在这个混乱的世界中，用心制作！"),
+        ("Slogan_tip", "宝鸡东站专用"),
         ("Privacy Statement", "隐私声明"),
         ("Mute", "静音"),
         ("Build Date", "构建日期"),
