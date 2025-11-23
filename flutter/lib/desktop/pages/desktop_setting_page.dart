288bc52cf6bf9c7f5a5dfb5f917152b72ad02363
yuanleezx <113503387+yuanleezx@users.noreply.github.com>
2025-11-23 19:59:50 +0800

    desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1636,14 +1636,14 @@ class _NetworkState extends State<_Network> with AutomaticKeepAliveClientMixin {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              if (!hideServer)
-                listTile(
-                  icon: Icons.dns_outlined,
-                  title: 'ID/Relay Server',
-                  onTap: () => showServerSettings(gFFI.dialogManager),
-                ),
-              if (!hideServer && (!hideProxy || !hideWebSocket))
-                Divider(height: 1, indent: 16, endIndent: 16),
+//              if (!hideServer)
+//                listTile(
+//                  icon: Icons.dns_outlined,
+//                  title: 'ID/Relay Server',
+//                  onTap: () => showServerSettings(gFFI.dialogManager),
+//                ),
+//              if (!hideServer && (!hideProxy || !hideWebSocket))
+//                Divider(height: 1, indent: 16, endIndent: 16),
               if (!hideProxy)
                 listTile(
                   icon: Icons.network_ping_outlined,
